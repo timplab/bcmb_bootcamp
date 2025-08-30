d2edd18633fad115e3e6c4ce9a2c154e4fe238b8
Winston <wtimp@jhu.edu>
2025-08-30 21:08:02 +0000

    timp_work_2025

---
+++ day3/notebooks/250827translate.py
@@ -0,0 +1,37 @@
+#This dictionary represents the 'genetic code' of DNA codons and their corresponding amino acids
+gencode = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+
+# First let's write a function to return the amino acid associated with a given codon.
+def lookup_AA(codon):
+    aa = gencode[codon]
+    return(aa)
+
+# Let's dissect this function here to see what's going on and then we can make a reuseable script out of this
+def translate_dna(dna):
+    myprotein=""
+    for i in range(0,len(dna), 3):
+        #print(myprotein)
+        codon=dna[i:i+3]
+        myprotein=myprotein+lookup_AA(codon)
+    return(myprotein)
+
+
+import sys
+
+print(translate_dna(sys.argv[1]))
